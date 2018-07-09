8e99a44755590678e1b265933ea2b44767ecf965
Siddharth <siddhartharora02@gmail.com>
2018-07-09 15:56:18 +0530

    changing api  baseURL for production

--- src/data/config.js
+++ src/data/config.js
@@ -1,6 +1,6 @@
 let baseURL = '';
 if(process.env.NODE_ENV === 'production'){
-  baseURL = 'https://siddhartharora.com/api'
+  baseURL = 'http://139.59.59.197:3001/'
 }else{
   baseURL = 'http://localhost:3000'
 }
